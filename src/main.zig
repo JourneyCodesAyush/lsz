4e155e0672f8f09c8b49111c1210bb644435820c
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2026-07-08 10:36:55 +0530

    feat: wire up -a/--all flag to list hidden files and directories
    Pass Config struct to PrintDirectoryContents as a parameter.

--- src/main.zig
+++ src/main.zig
@@ -7,6 +7,7 @@ const list = @import("list.zig");
 pub fn main(init: std.process.Init) !void {
     const params = comptime clap.parseParamsComptime(
         \\-h, --help             Display this help and exit
+        \\-a, --all              Do not ignore entries starting with .
         \\<str>...               Files or directories to list 
     );
 
@@ -24,14 +25,17 @@ pub fn main(init: std.process.Init) !void {
     defer res.deinit();
 
     if (res.args.help != 0)
+        // return clap.usageToFile(init.io, .stderr(), clap.Help, &params);
         return clap.helpToFile(init.io, .stderr(), clap.Help, &params, .{});
+
+    const config: list.Config = list.Config{ .all = res.args.all != 0 };
 
     const path_count: u64 = res.positionals[0].len;
 
     var printDirectory: list.PrintDirectoryContents = undefined;
     defer printDirectory.deinit();
 
-    try printDirectory.init(init.io, @constCast(&allocator));
+    try printDirectory.init(init.io, @constCast(&allocator), config);
 
     if (path_count == 0) {
         try printDirectory.printDirectories(".");
